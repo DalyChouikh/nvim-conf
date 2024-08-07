83f8c4bade2ae6c0761c5876387f5a92eda121c9
DalyChouikh <chouikhdaly215@gmail.com>
2024-08-07 12:30:19 +0100

    Added keymaps for searching todos in the trouble list and telescope

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -16,3 +16,15 @@ keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close tab' })
 keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' })
 keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' })
 keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' })
+
+-- Jump to next TODO comment
+keymap.set('n', ']t', function()
+  require('todo-comments').jump_next()
+end, { desc = 'Next todo comment' })
+
+keymap.set('n', '[t', function()
+  require('todo-comments').jump_prev()
+end, { desc = 'Previous todo comment' })
+
+keymap.set('n', '<leader>st', '<cmd>TodoTelescope<cr>', { desc = '[S]earch Todos in Telescope' })
+keymap.set('n', '<leader>tt', '<cmd>TodoTrouble<cr>', { desc = '[S]earch Todos in Trouble list' })
